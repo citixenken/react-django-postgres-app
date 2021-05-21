4c26636305682b2fed5c504be9746bf2b01b3ebd
citixenken <kmuyesu@gmail.com>
2021-05-21 10:51:44 +0300

    JSON Web Token Customization

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,9 +3,36 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .products import products
 from .models import *
 from .serializers import *
+
+#JWT customization
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['message'] = 'hello there'
+        # ...
+
+        return token
+    
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 # Create your views here.
 @api_view(['GET'])
