14ea20f1fbcf0bb8c7e53f1fedf06539db75773c
citixenken <kmuyesu@gmail.com>
2021-05-25 21:02:38 +0300

    Update Profile backend

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,5 +6,6 @@ urlpatterns =[
     path('login/', user_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', user_views.registerUser, name = 'register-user'),
     path('profile/', user_views.getUserProfile, name = 'users-profile'),
+    path('profile/update/', user_views.updateUserProfile, name = 'users-profile-update'),
     path('', user_views.getUsers, name = 'users'),
 ]
