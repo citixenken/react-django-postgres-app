63d6383aba166d30bb6b424788c9dd09324ad3e0
citixenken <kmuyesu@gmail.com>
2021-06-02 13:52:52 +0300

    Paypal and Debit/Credit integration

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -16,17 +16,24 @@ import Message from "../components/Message";
 import FormContainer from "../components/FormContainer";
 
 import { useDispatch, useSelector } from "react-redux";
-import { getOrderDetails } from "../actions/orderActions";
+import { PayPalButton } from "react-paypal-button-v2";
+import { getOrderDetails, payOrder } from "../actions/orderActions";
+import { ORDER_PAY_RESET } from "../constants/orderConstants";
 
 const OrderScreen = ({ match }) => {
     const orderId = match.params.id;
 
     const dispatch = useDispatch();
 
+    const [sdkReady, setSdkReady] = useState(false);
+
     const orderDetails = useSelector((state) => state.orderDetails);
 
     //destructuring orderScreen
     const { order, error, loading } = orderDetails;
+
+    const orderPay = useSelector((state) => state.orderPay);
+    const { loading: loadingPay, success: successPay } = orderPay;
 
     if (!loading && !error) {
         //calculations below are dynamic for this page ONLY!
@@ -44,11 +51,36 @@ const OrderScreen = ({ match }) => {
         ).toFixed(2);
     }
 
+    //ClientID:
+    const addPayPalScript = () => {
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src =
+            "https://www.paypal.com/sdk/js?client-id=AWNhvGZ2tBPBAajiX5oczYMrpBN1StEeg_ZkutQzeY40dsMLTG7tDXy_qOoGi0w8POsP1xv0itUduJyg";
+        script.async = true;
+        script.onload = () => {
+            setSdkReady(true);
+        };
+        document.body.appendChild(script);
+    };
+
     useEffect(() => {
-        if (!order || order._id !== Number(orderId)) {
+        if (!order || successPay || order._id !== Number(orderId)) {
+            dispatch({ type: ORDER_PAY_RESET });
+
             dispatch(getOrderDetails(orderId));
+        } else if (!order.isPaid) {
+            if (!window.paypal) {
+                addPayPalScript();
+            } else {
+                setSdkReady(true);
+            }
         }
-    }, [dispatch, order, orderId]);
+    }, [dispatch, order, orderId, successPay]);
+
+    const successPaymentHandler = (paymentResult) => {
+        dispatch(payOrder(orderId, paymentResult));
+    };
 
     return loading ? (
         <Loader />
@@ -184,6 +216,20 @@ const OrderScreen = ({ match }) => {
                                     <Col>${order.totalPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
+
+                            {!order.isPaid && (
+                                <ListGroup.Item>
+                                    {loadingPay && <Loader />}
+                                    {!sdkReady ? (
+                                        <Loader />
+                                    ) : (
+                                        <PayPalButton
+                                            amount={order.totalPrice}
+                                            onSuccess={successPaymentHandler}
+                                        ></PayPalButton>
+                                    )}
+                                </ListGroup.Item>
+                            )}
                         </ListGroup>
                     </Card>
                 </Col>
