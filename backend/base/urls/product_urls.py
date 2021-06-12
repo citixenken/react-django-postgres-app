ec82ec1d4cc2c132904fb7ee6392a18470c848e5
citixenken <kmuyesu@gmail.com>
2021-06-12 22:36:03 +0300

    Create and Update Products Endpoints

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -5,6 +5,7 @@ from base.views import product_views
 urlpatterns =[
         
     path('', product_views.getProducts, name = 'products'),
+    path('create/<str:pk>/', product_views.createProduct, name = 'create-product'),
     path('<str:pk>/', product_views.getProduct, name = 'product'),
     path('delete/<str:pk>/', product_views.deleteProduct, name = 'delete-product'),
 ]
--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -34,6 +34,42 @@ def getProduct(request, pk):
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data) 
 
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    user = request.user
+    
+    product= Product.objects.create(
+        user = user,
+        name = 'name',
+        price = 0,
+        brand='brand',
+        countInStock = 0,
+        category = 'category',
+        description = '',
+    )
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data) 
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    data = request.data
+    product = Product.objects.get(_id = pk)
+
+    product.name = data['name']
+    product.price = data['price']
+    product.brand = data['brand']
+    product.countInStock = data['countInStock']
+    product.category = data['category']
+    product.description = data['description']
+
+    product.save()
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data) 
+
 @api_view(['DELETE'])
 @permission_classes([IsAdminUser])
 def deleteProduct(request, pk):
@@ -41,3 +77,5 @@ def deleteProduct(request, pk):
     product = Product.objects.get(_id = pk)
     product.delete()
     return Response("Product Deleted!")
+
+
