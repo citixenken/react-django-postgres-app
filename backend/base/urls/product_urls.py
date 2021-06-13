2d2448534e3eafa292d29a3ca74c6f7990f8e7cb
citixenken <kmuyesu@gmail.com>
2021-06-14 00:25:01 +0300

    Product Image Upload

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -6,6 +6,7 @@ urlpatterns =[
         
     path('', product_views.getProducts, name = 'products'),
     path('create/', product_views.createProduct, name = 'create-product'),
+    path('upload/', product_views.uploadImage, name = 'upload-image'),
     path('<str:pk>/', product_views.getProduct, name = 'product'),
     path('update/<str:pk>/', product_views.updateProduct, name = 'update-product'),
     
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import {
     Row,
@@ -34,6 +35,7 @@ const ProductEditScreen = ({ match, history }) => {
     const [category, setCategory] = useState("");
     const [countInStock, setCountInStock] = useState(0);
     const [description, setDescription] = useState("");
+    const [uploading, setUploading] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -82,6 +84,34 @@ const ProductEditScreen = ({ match, history }) => {
         );
     };
 
+    //upload file handler
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0];
+        const formData = new FormData();
+
+        formData.append("image", file);
+        formData.append("product_id", productId);
+
+        setUploading(true);
+
+        try {
+            const config = {
+                headers: { "Content-Type": "multipart/form-data" },
+            };
+
+            const { data } = await axios.post(
+                "/api/products/upload/",
+                formData,
+                config
+            );
+
+            setImage(data);
+            setUploading(false);
+        } catch (error) {
+            setUploading(false);
+        }
+    };
+
     return (
         <div>
             <Link to="/admin/productlist">Go Back</Link>
@@ -126,6 +156,13 @@ const ProductEditScreen = ({ match, history }) => {
                                 value={image}
                                 onChange={(e) => setImage(e.target.value)}
                             ></Form.Control>
+                            <Form.File
+                                id="image-file"
+                                label="Choose File"
+                                custom
+                                onChange={uploadFileHandler}
+                            ></Form.File>
+                            {uploading && <Loader />}
                         </Form.Group>
 
                         <Form.Group controlId="brand">
